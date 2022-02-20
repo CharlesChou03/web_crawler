3bfdd9a54626e7c6b777ecc51196ad876265d347
Charles Chou <tycoon0049@gmail.com>
2022-02-20 19:28:28 +0800

    add review feature

--- English_learn/create_vocabulary.py
+++ English_learn/create_vocabulary.py
@@ -3,10 +3,19 @@ from bs4 import BeautifulSoup
 import os
 
 NOT_FOUND_MESSAGE = "Did you mean:"
-FILE_PATH = "./vocabulary.txt"
 
 print("Please input vocabulary: ", end='')
 vocabulary = input()
+print("Please input destination (n or r): ", end='')
+destination = input()
+if destination == "n":
+    FILE_PATH = "./vocabulary.txt"
+elif destination == "r":
+    FILE_PATH = "./review_vocabulary.txt"
+else:
+    print("Please input valid destination! (n for normal or r for review)")
+    exit(0)
+print("Execute file path is \"%s\" by destination \"%s\"" % (FILE_PATH, destination))
 # https://blog.csdn.net/Max_R/article/details/30219035
 if not vocabulary.encode( 'UTF-8' ).isalpha():
     print("Please input valid vocabulary!")
--- English_learn/display_vocabulary.py
+++ English_learn/display_vocabulary.py
@@ -1,7 +1,6 @@
 import random
 import os
 
-FILE_PATH = "./vocabulary.txt"
 DEFAULT_N = 3
 
 print("Please input vocabulary number you wanna see: ", end='')
@@ -15,6 +14,17 @@ if not inp:
     n = DEFAULT_N
 else:
     n = int(inp)
+
+print("Please input destination (n or r): ", end='')
+destination = input()
+if destination == "n":
+    FILE_PATH = "./vocabulary.txt"
+elif destination == "r":
+    FILE_PATH = "./review_vocabulary.txt"
+else:
+    print("Please input valid destination! (n for normal or r for review)")
+    exit(0)
+print("Execute file path is \"%s\" by destination \"%s\"" % (FILE_PATH, destination))
 
 if not os.path.isfile(FILE_PATH):
     print("Please create \"vocabulary.txt\" first!")
